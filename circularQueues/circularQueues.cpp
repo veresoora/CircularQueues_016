8acbd749fad63cd2c2c87e8b0fad17f92607204a
veresoora <lahanabira@gmail.com>
2023-06-22 10:49:06 +0700

    memasukkan kondisi jika rear berada diposisi terakhir array, kembali ke awal array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -28,6 +28,13 @@ public:
 			FRONT = 0;
 			REAR = 0;
 		}
-		
+		else {
+			//Jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
