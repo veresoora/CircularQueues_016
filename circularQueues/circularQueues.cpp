ef0213f3e6579fd0bc638d928bb3a8a5a881c8c6
veresoora <lahanabira@gmail.com>
2023-06-22 11:04:25 +0700

    mengecek apakah antrian kosong

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -63,6 +63,12 @@ public:
 	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
 	}
 	
 };
