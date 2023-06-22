375ebce52104ff0e24e06c0801e8146b999b61f6
veresoora <lahanabira@gmail.com>
2023-06-22 11:14:52 +0700

    Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -80,6 +80,14 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			//Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+		}
 	}
 	
 };
