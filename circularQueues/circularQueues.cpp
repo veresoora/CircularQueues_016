91746467f9bd9ab4a3630a5a25de17a66c725127
veresoora <lahanabira@gmail.com>
2023-06-22 10:35:10 +0700

    menambakan public pada class queues

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class Queues{
+class Queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
-
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
 }
