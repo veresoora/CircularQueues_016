15e7da1e492bd2307346b5b079d878320222b264
veresoora <lahanabira@gmail.com>
2023-06-22 11:00:46 +0700

    memasukkan kondisi jika elemen yang dihapus berada diposisi terakhir array, kembali ke awal array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -51,6 +51,13 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			//Jika elemen yang dihapus berada diposisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 	
 };
