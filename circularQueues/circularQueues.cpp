37f67e3f247903a0cae983e4f5801db2107fa83f
veresoora <lahanabira@gmail.com>
2023-06-22 11:09:50 +0700

    Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -69,6 +69,17 @@ public:
 			cout << "Queue is empty\n";
 			return;
 		}
+
+		cout << "\nElements in the queue are: \n";
+
+		//Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
 	
 };
